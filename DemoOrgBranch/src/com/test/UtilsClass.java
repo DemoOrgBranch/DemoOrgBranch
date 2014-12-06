4a448794a1216aab894c451320fecf61e6659101
carAmeen <ameen.5504@gmail.com>
2014-12-06 13:19:54 +0530

    To test conflicts

--- DemoOrgBranch/src/com/test/UtilsClass.java
+++ DemoOrgBranch/src/com/test/UtilsClass.java
@@ -6,6 +6,6 @@ public class UtilsClass {
 		System.out.println("Utils");
 		System.out.println("Utils add in nav 1 branch");
 		System.out.println("Utils jjjj");
-		System.out.println("Utils added from nav branch");
+		System.out.println("Utils added from nav branch.. to test conflicsssss addeb from carameen");
 	}
 }
