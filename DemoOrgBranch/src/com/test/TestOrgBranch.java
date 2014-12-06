b970fa01cd0a86536c6e2dbf4d14650429803fc1
pkrnavin <pkrnavin@gmail.com>
2014-12-06 14:39:13 +0530

    after resloving conflict from ameen

--- DemoOrgBranch/src/com/test/TestOrgBranch.java
+++ DemoOrgBranch/src/com/test/TestOrgBranch.java
@@ -3,6 +3,6 @@ package com.test;
 public class TestOrgBranch {
 	public static void main(String[] args) {
 		System.out.println("Organisation ");
-		System.out.println("Commit from Nav branch 11111 ghghg... To test conflict..."); 
+		System.out.println("Commit from Nav branch 11111 ghghg... To test conflict...after resolving conflict from ammen"); 
 	}
 }
